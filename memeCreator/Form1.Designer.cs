f0f5b011cbb50beea76f9d71892a335b97d5b112
Verna Marie Maullon <59632000+mathstudent97@users.noreply.github.com>
2021-03-13 23:28:24 -0500

    Add files via upload

---
+++ memeCreator/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace memeCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.topText = new System.Windows.Forms.TextBox();
+            this.bottomText = new System.Windows.Forms.TextBox();
+            this.topLabel = new System.Windows.Forms.Label();
+            this.bottomLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(213, 360);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(397, 360);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bottomText);
+            this.groupBox1.Controls.Add(this.topText);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 352);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Top Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bottom Text";
+            // 
+            // topText
+            // 
+            this.topText.Location = new System.Drawing.Point(51, 90);
+            this.topText.Multiline = true;
+            this.topText.Name = "topText";
+            this.topText.Size = new System.Drawing.Size(254, 71);
+            this.topText.TabIndex = 2;
+            this.topText.TextChanged += new System.EventHandler(this.topText_TextChanged);
+            // 
+            // bottomText
+            // 
+            this.bottomText.Location = new System.Drawing.Point(51, 267);
+            this.bottomText.Multiline = true;
+            this.bottomText.Name = "bottomText";
+            this.bottomText.Size = new System.Drawing.Size(254, 62);
+            this.bottomText.TabIndex = 3;
+            // 
+            // topLabel
+            // 
+            this.topLabel.AutoSize = true;
+            this.topLabel.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLabel.ForeColor = System.Drawing.Color.White;
+            this.topLabel.Location = new System.Drawing.Point(392, 24);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(66, 26);
+            this.topLabel.TabIndex = 4;
+            this.topLabel.Text = "label3";
+            this.topLabel.Click += new System.EventHandler(this.topLabel_Click);
+            // 
+            // bottomLabel
+            // 
+            this.bottomLabel.AutoSize = true;
+            this.bottomLabel.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomLabel.ForeColor = System.Drawing.Color.White;
+            this.bottomLabel.Location = new System.Drawing.Point(423, 319);
+            this.bottomLabel.Name = "bottomLabel";
+            this.bottomLabel.Size = new System.Drawing.Size(65, 26);
+            this.bottomLabel.TabIndex = 5;
+            this.bottomLabel.Text = "label4";
+            this.bottomLabel.Click += new System.EventHandler(this.bottomLabel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(397, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(373, 342);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bottomLabel);
+            this.Controls.Add(this.topLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox bottomText;
+        private System.Windows.Forms.TextBox topText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Label bottomLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ memeCreator/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memeCreator
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openImage; // Var.that opens image file. Idea: this finds an img file and stores its location inside
+        // the image file str to be used later.
+        string imageFile; // String that will name the image file.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openImage = new OpenFileDialog(); // Creates open file dialog class to select a file from windows index.
+            openImage.InitialDirectory = "C:\\"; // Sets directory to C (local harddrive) to navigate.
+            openImage.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png; *.gif; *.bmp";
+            // Above line filters the files; showing ONLY image files.
+            openImage.FilterIndex = 2; // Reps. index of filter currently selected in dialog box.
+            openImage.RestoreDirectory = true; // When close FileDialog, original working directory is restored.
+
+            if (openImage.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    preview.Image = Image.FromFile(openImage.FileName);
+                    imageFile = openImage.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file. Original error " + ex.Message);
+                }
+            }
+        }
+
+
+        private void topText_TextChanged(object sender, EventArgs e)
+        {
+            // Want: topLabel to change with the topText box.
+            topLabel.Text = topText.Text;
+        }
+
+        private void bottomLabel_Click(object sender, EventArgs e)
+        {
+            bottomLabel.Text = bottomText.Text; // Apply same from above.
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
